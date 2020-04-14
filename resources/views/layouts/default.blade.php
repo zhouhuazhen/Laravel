6a9dc710dc6eb2a36d90a666c6d9784e3e1c0d20
zhouhuazhen <382938018@qq.com>
2020-04-14 16:52:07 +0800

    基础页面的添加

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>@yield('title', 'weibo App')- Laravel 新手入门教程</title>
+</head>
+<body>
+  @yield('content')
+</body>
+</html>
