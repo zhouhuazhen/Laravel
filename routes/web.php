9cbea8ad652b6f7a59d0580498b0d2fea0461f0e
zhouhuazhen <382938018@qq.com>
2020-04-16 15:16:04 +0800

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,4 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
  * 注册路由
  */
 Route::get('signup', 'UserController@create')->name('signup');
+Route::resource('users', 'UserController');
