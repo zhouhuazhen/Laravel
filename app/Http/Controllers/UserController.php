309affbfcb36171b5e9ad78666ec689a8d9566a7
zhouhuazhen <382938018@qq.com>
2020-04-21 11:23:40 +0800

    更改用户资料

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -37,4 +37,36 @@ class UserController extends Controller
       session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
       return redirect()->route('users.show', [$user]);
     }
+
+    //编辑用户-编辑用户页面
+    public function edit(User $user){
+
+      return view('users.edit', compact('user'));
+
+    }
+
+    //更新用户信息
+    public function update(User $user, Request $request){
+
+      $this->validate($request, [
+         'name' => 'required|max:50',
+         'password' => 'nullable|confirmed|min:6'
+      ]);
+
+      $data = [];
+      $data['name'] = $request->name;
+      if($request->password){
+        $data['password'] = bcrypt($request->password);
+      }
+      $user->update($data);
+
+      // $user->update([
+      //    'name' => $request->name,
+      //    'password' => bcrypt($request->password),
+      // ]);
+
+      session()->flash('success', '个人资料更新成功');
+
+      return redirect()->route('users.show', $user->id);
+    }
 }
