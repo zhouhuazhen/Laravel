18278ced6086a9375b7108b37d88b2537baa0e14
zhouhuazhen <382938018@qq.com>
2020-04-21 14:43:15 +0800

    管理员删除用户功能的添加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -87,7 +87,16 @@ class UserController extends Controller
     //显示所有的用户
     public function index(){
       // $users = User::all();
-      $users = User::paginate(10);
+      $users = User::orderByDesc('id')->paginate(10);
       return view('users.index', compact('users'));
+    }
+
+    //删除用户
+    public function destroy(User $user){
+
+      $this->authorize('destroy', $user);
+      $user->delete();
+      session()->flash('success', '成功删除用户！');
+      return back();
     }
 }
