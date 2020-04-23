bbec50d1488caa51f9929f0b67e1c69fa94a1105
zhouhuazhen <382938018@qq.com>
2020-04-23 15:13:04 +0800

    用户微博列表

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,17 +5,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-      Model::unguard();
+        Model::unguard();
 
-      $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(StatusesTableSeeder::class);
 
-      Model::reguard();
+        Model::reguard();
     }
 }
